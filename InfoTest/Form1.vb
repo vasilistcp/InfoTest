178c744ed81423b1e28d043f3453a9a5366e6974
vasilistcp <vasilistcp@outlook.com>
2017-10-24 14:14:10 +0300

    third change

--- InfoTest/Form1.vb
+++ InfoTest/Form1.vb
@@ -2,5 +2,7 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim a As String
         Dim b As Integer
+        Dim c As Decimal
+
     End Sub
 End Class
